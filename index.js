37a08ad82f47a6418555ec6cd22143d9ec6e00a0
BrennanB572 <Brennanb572@gmail.com>
2023-05-16 21:51:24 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
 /* Your Code Here */
+function createEmployeeRecord(data){
+    return {
+        firstName: data[0],
+        familyName: data[1],
+        title: data[2],
+        payPerHour: data[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+};
+
+function createEmployeeRecords(employees){
+    return employees.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(date){
+    const timeInEvent = {
+        type: "TimeIn",
+        hour: Number(date.slice(-4)),
+        date: date.slice(0, 10)
+    }
+    this.timeInEvents.push(timeInEvent);
+    return this;
+}
+
+function createTimeOutEvent(date){
+    const timeOutEvent = {
+        type: "TimeOut",
+        hour: Number(date.slice(-4)),
+        date: date.slice(0, 10)
+    }
+    this.timeOutEvents.push(timeOutEvent);
+    return this;
+}
+
+function hoursWorkedOnDate(date){
+    const timeIn = this.timeInEvents.find(em => em.date === date);
+    const timeOut = this.timeOutEvents.find(em => em.date === date);
+
+    const calcHours = (timeOut.hour - timeIn.hour) / 100;
+    return calcHours;
+}
+
+function wagesEarnedOnDate(date){
+    return this.payPerHour * hoursWorkedOnDate.call(this, date);
+}
+
+function findEmployeeByFirstName(arr, firstName){
+    return arr.find(e => e.firstName === firstName);
+}
+
+function calculatePayroll(aaRecords){
+    return aaRecords.reduce((a, b) => allWagesFor.call(b) + a, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +74,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
